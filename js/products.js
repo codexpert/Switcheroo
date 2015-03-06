7290e6d989cdd688c28cd5147c0cbf2094ca89de
Parvez Akther <parvez@themexpert.com>
2015-03-06 18:25:58 +0600

    2013 templates added

--- js/products.js
+++ js/products.js
@@ -93,5 +93,93 @@ $products = {
 		purchase : 'http://themexpert.com/joomla-templates/boost',
 		tooltip  : 'Ecommerce Template'
 	},
+	bolt : {
+		name     : 'Bolt',
+		tag      : 'Joomla 3.x,Joomla 2.5,Multipurpose',
+		img      : 'http://demo.themexpert.com/images/bolt.jpg',
+		url      : 'http://bolt.demo.themexpert.com',
+		purchase : 'http://themexpert.com/joomla-templates/bolt',
+		tooltip  : 'Business, Travel, Event Template'
+	},
+	optimus : {
+		name     : 'Optimus',
+		tag      : 'Joomla 3.x,Joomla 2.5,Business',
+		img      : 'http://demo.themexpert.com/images/optimus.jpg',
+		url      : 'http://optimus.demo.themexpert.com',
+		purchase : 'http://themexpert.com/joomla-templates/optimus',
+		tooltip  : 'Corporate Business Template'
+	},
+	gravityii : {
+		name     : 'Gravityii',
+		tag      : 'Joomla 3.x,Joomla 2.5,Ecommerce',
+		img      : 'http://demo.themexpert.com/images/gravityii.jpg',
+		url      : 'http://gravityii.demo.themexpert.com',
+		purchase : 'http://themexpert.com/joomla-templates/gravityii',
+		tooltip  : 'Ecommerce Template'
+	},
+	photon : {
+		name     : 'Photon',
+		tag      : 'Joomla 3.x,Joomla 2.5,App',
+		img      : 'http://demo.themexpert.com/images/photon.jpg',
+		url      : 'http://photon.demo.themexpert.com',
+		purchase : 'http://themexpert.com/joomla-templates/photon',
+		tooltip  : 'App Landing Page Template'
+	},
+	focus : {
+		name     : 'Focus',
+		tag      : 'Joomla 3.x,Joomla 2.5,Education',
+		img      : 'http://demo.themexpert.com/images/focus.jpg',
+		url      : 'http://focus.demo.themexpert.com',
+		purchase : 'http://themexpert.com/joomla-templates/focus',
+		tooltip  : 'Education and Social Template'
+	},
+	crunch : {
+		name     : 'Crunch',
+		tag      : 'Joomla 3.x,Joomla 2.5,Business',
+		img      : 'http://demo.themexpert.com/images/crunch.jpg',
+		url      : 'http://crunch.demo.themexpert.com',
+		purchase : 'http://themexpert.com/joomla-templates/crunch',
+		tooltip  : 'Business Template'
+	},
+	cohesion : {
+		name     : 'Cohesion',
+		tag      : 'Joomla 3.x,Joomla 2.5,Business',
+		img      : 'http://demo.themexpert.com/images/cohesion.jpg',
+		url      : 'http://cohesion.demo.themexpert.com',
+		purchase : 'http://themexpert.com/joomla-templates/cohesion',
+		tooltip  : 'Business Template'
+	},
+	zenith : {
+		name     : 'Zenith',
+		tag      : 'Joomla 3.x,Joomla 2.5,Free',
+		img      : 'http://demo.themexpert.com/images/zenith.jpg',
+		url      : 'http://zenith.demo.themexpert.com',
+		purchase : 'http://themexpert.com/joomla-templates/zenith',
+		tooltip  : 'Business Template'
+	},
+	delicious : {
+		name     : 'Delicious',
+		tag      : 'Joomla 3.x,Joomla 2.5,Ecommerce',
+		img      : 'http://demo.themexpert.com/images/delicious.jpg',
+		url      : 'http://delicious.demo.themexpert.com',
+		purchase : 'http://themexpert.com/joomla-templates/delicious',
+		tooltip  : 'Ecommerce Template'
+	},
+	simplex : {
+		name     : 'Simplex',
+		tag      : 'Joomla 3.x,Joomla 2.5,Business',
+		img      : 'http://demo.themexpert.com/images/simplex.jpg',
+		url      : 'http://simplex.demo.themexpert.com',
+		purchase : 'http://themexpert.com/joomla-templates/simplex',
+		tooltip  : 'Business Template'
+	},
+	extreme : {
+		name     : 'Extreme',
+		tag      : 'Joomla 3.x,Joomla 2.5,Business',
+		img      : 'http://demo.themexpert.com/images/extreme.jpg',
+		url      : 'http://extreme.demo.themexpert.com',
+		purchase : 'http://themexpert.com/joomla-templates/extreme',
+		tooltip  : 'Gaming Template'
+	},
 
 };
