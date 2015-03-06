db362e39837a156aa4a0e83e25d95158252a37ba
Parvez Akther <parvez@themexpert.com>
2015-03-06 12:20:20 +0600

    added all 2014 products

--- js/products.js
+++ js/products.js
@@ -37,5 +37,61 @@ $products = {
 		purchase : 'http://themexpert.com/joomla-templates/event',
 		tooltip  : 'Event Template'
 	},
+	nefario : {
+		name     : 'Nefario',
+		tag      : 'Joomla 3.x,Joomla 2.5,Business',
+		img      : 'http://demo.themexpert.com/images/nefario.jpg',
+		url      : 'http://nefario.demo.themexpert.com/',
+		purchase : 'http://themexpert.com/joomla-templates/nefario',
+		tooltip  : 'Corporate Business Template'
+	},
+	eduxpert : {
+		name     : 'EduXpert',
+		tag      : 'Joomla 3.x,Joomla 2.5,Education',
+		img      : 'http://demo.themexpert.com/images/eduxpert.jpg',
+		url      : 'http://eduxpert.demo.themexpert.com/',
+		purchase : 'http://themexpert.com/joomla-templates/eduxpert',
+		tooltip  : 'Education Template'
+	},
+	sportz : {
+		name     : 'Sportz',
+		tag      : 'Joomla 3.x,Joomla 2.5,News',
+		img      : 'http://demo.themexpert.com/images/sportz.jpg',
+		url      : 'http://sportz.demo.themexpert.com/',
+		purchase : 'http://themexpert.com/joomla-templates/sportz',
+		tooltip  : 'News and Magazine Template'
+	},
+	zenithii : {
+		name     : 'ZenithII',
+		tag      : 'Joomla 3.x,Joomla 2.5,Free',
+		img      : 'http://demo.themexpert.com/images/zenithii.jpg',
+		url      : 'http://zenithii.demo.themexpert.com/',
+		purchase : 'http://themexpert.com/joomla-templates/zenithii',
+		tooltip  : 'Business Template'
+	},
+	metronews : {
+		name     : 'Metronews',
+		tag      : 'Joomla 3.x,Joomla 2.5,News',
+		img      : 'http://demo.themexpert.com/images/metronews.jpg',
+		url      : 'http://metronews.demo.themexpert.com/',
+		purchase : 'http://themexpert.com/joomla-templates/metronews',
+		tooltip  : 'News Template'
+	},
+	appy : {
+		name     : 'Appy',
+		tag      : 'Joomla 3.x,Joomla 2.5,Business',
+		img      : 'http://demo.themexpert.com/images/appy.jpg',
+		url      : 'http://appy.demo.themexpert.com/',
+		purchase : 'http://themexpert.com/joomla-templates/appy',
+		tooltip  : 'App Landing Page Template'
+	},
+	boost : {
+		name     : 'Boost',
+		tag      : 'Joomla 3.x,Joomla 2.5,Ecommerce',
+		img      : 'http://demo.themexpert.com/images/boost.jpg',
+		url      : 'http://boost.demo.themexpert.com/',
+		purchase : 'http://themexpert.com/joomla-templates/boost',
+		tooltip  : 'Ecommerce Template'
+	},
 
 };
